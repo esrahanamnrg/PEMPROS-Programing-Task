de51d4a31671c7661dde06fc96c1877fc9e079df
esrahanamnrg <70983316+esrahanamnrg@users.noreply.github.com>
2022-08-08 20:41:00 +0700

    Add files via upload

---
+++ t08/2021-ge-t08-academic-simulator-esrahanamnrg/custom.h
@@ -0,0 +1,12 @@
+// 12S20050 - Putri Esrahana
+
+#include "academic.h"
+
+#ifndef CUSTOM_H
+#define CUSTOM_H
+
+enum grade_t gradeEnum(char *grade);
+enum semester_t semesterEnum(char *semester);
+int index_student(struct student_t *_students, unsigned short int _student_size, char *_id);
+
+#endif
